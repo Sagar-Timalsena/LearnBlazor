930f143e3caeb90c12043612fca990bdc4e32c47
Sagar-Timalsena <stimalsena155@gmail.com>
2022-12-15 20:32:45 +0545

    completed dropdown

--- Tangy_Models/LeanBlazorModels/Demo_ProductProp.cs
+++ Tangy_Models/LeanBlazorModels/Demo_ProductProp.cs
@@ -9,8 +9,8 @@ namespace Tangy_Models.LeanBlazorModels
     public class Demo_ProductProp
     {
         public int Id { get; set; }
-        public string key { get; set; }
-        public string value { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
        
 
     }
