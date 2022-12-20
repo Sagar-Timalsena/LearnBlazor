91d2b0b5395925a5ed246ab7ab84e702fc9916cd
Sagar-Timalsena <stimalsena155@gmail.com>
2022-12-20 09:30:46 +0545

    assignment mistake

--- Tangy_Models/LeanBlazorModels/Demo_Product.cs
+++ Tangy_Models/LeanBlazorModels/Demo_Product.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tangy_Models.LeanBlazorModels
+﻿namespace Tangy_Models.LeanBlazorModels
 {
     public class Demo_Product
     {
